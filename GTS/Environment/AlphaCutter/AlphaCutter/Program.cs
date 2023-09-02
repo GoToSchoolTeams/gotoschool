8793c16f94ff6380bf8305a49eea623af732ed79
Kerotanius <95579929+Kerotanius@users.noreply.github.com>
2023-09-03 00:33:02 +0900

    画像切り取りツールコミット

---
+++ GTS/Environment/AlphaCutter/AlphaCutter/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ディレクトリのパスを引数で指定
+        var directoryPath = args[0];
+
+        // ディレクトリ内のすべてのファイルを取得
+        string[] fileNames = Directory.GetFiles(directoryPath);
+
+        // ファイル名をコンソールに出力
+        for(var i = 0; i < fileNames.Length; i++)
+        {
+            var fileName = fileNames[i];
+            var file_type = Path.GetExtension(fileName);
+            var dir_path = Path.GetDirectoryName(fileName);
+            var name = Path.GetFileName(fileName);
+            if (file_type.Contains("png") == true)
+            {
+                //! 画像の読み込み
+                using (var orig_image = new Bitmap(fileName))
+                {
+                    //! 透明じゃない領域の計算
+                    var target_rect = GetRect(orig_image);
+
+                    //! 空オブジェクトだった = 変換不可なのでskip
+                    if (target_rect == Rectangle.Empty) continue;
+
+                    //! 計算された領域で画像を切り取る
+                    using (var dest_image = orig_image.Clone(target_rect, orig_image.PixelFormat))
+                    {
+                        //! プリフィックスをつけて保存
+                        var new_name = Path.Combine(dir_path, $"cut_{name}");
+                        dest_image.Save(new_name);
+                    }
+                }
+            }
+        }
+    }
+
+    static Rectangle GetRect(Bitmap bmp)
+    {
+        // 画像のピクセルを byte[] にコピーする
+        var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+        var bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly, bmp.PixelFormat);
+        var bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+        var rgbValues = new byte[bytes];
+        System.Runtime.InteropServices.Marshal.Copy(bmpData.Scan0, rgbValues, 0, bytes);
+        bmp.UnlockBits(bmpData);
+
+        int x0 = bmp.Width;
+        int y0 = bmp.Height;
+        int x1 = 0;
+        int y1 = 0;
+
+        bool isCutable = false;
+
+        // 透明でないピクセルを探す
+        for (int i = 3; i < rgbValues.Length; i += 4)
+        {
+            // Aの値が0なら透明ピクセル
+            if (rgbValues[i] != 0)
+            {
+                isCutable = true;
+                int x = i / 4 % bmp.Width;
+                int y = i / 4 / bmp.Width;
+
+                if (x0 > x) x0 = x;
+                if (y0 > y) y0 = y;
+                if (x1 < x) x1 = x;
+                if (y1 < y) y1 = y;
+            }
+        }
+
+        if(isCutable)
+        {
+            return new Rectangle(x0, y0, x1 - x0, y1 - y0);
+        }
+        else
+        {
+            return Rectangle.Empty;
+        }
+    }
+}
---
+++ GTS/Environment/formatter/nobust.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"unsafe"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+)
+
+const dest = "./"
+
+var MAIN_CHARACTER_NAMES = [14]string{
+	"大樹",
+	"遥香",
+	"縁",
+	"悟",
+	"ガイア",
+	"マッシュ",
+	"オルテガ",
+	"十二単",
+	"マル",
+	"松戸",
+	"高橋",
+	"成瀬",
+	"御厨",
+	"聡",
+}
+
+func main() {
+	//! ファイル名を抜き出して宛先ファイルを作成
+	var source = os.Args[1]
+	var basename = filepath.Base(source)
+	basename = strings.Split(basename, ".")[0]
+	dist, err := os.Create(dest + "/" + basename + ".ks")
+	defer dist.Close()
+
+	//! 引数で指定されたシナリオを読み込む
+	f, err := os.Open(source)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer f.Close()
+
+	//! 宛先ファイルに内容を丸コピ
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	//! オリジナルはShift-JISなのでデコード
+	reader := transform.NewReader(bytes.NewReader(b), japanese.ShiftJIS.NewDecoder())
+	orig, err := ioutil.ReadAll(reader)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	origStr := *(*string)(unsafe.Pointer(&orig))
+
+	//! コピー予定テキストに処理を実施
+	addPLN(&origStr)
+	addMWindowMacro(&origStr)
+	addLR(&origStr)
+
+	//! コピー予定テキストをshift-jisに変換して書き込み
+	reader = transform.NewReader(strings.NewReader(origStr), japanese.ShiftJIS.NewEncoder())
+	origJis, err := ioutil.ReadAll(reader)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	dist.Write(origJis)
+}
+
+func addPLN(dist *string) {
+	//! セリフ、語りのダブル改行にタグを仕込む
+	*(dist) = strings.ReplaceAll(*dist, "。\r\n\r\n", "。[plc2]\r\n\r\n")
+	*(dist) = strings.ReplaceAll(*dist, "」\r\n\r\n", "」[plc2]\r\n\r\n")
+	*(dist) = strings.ReplaceAll(*dist, "？\r\n\r\n", "？[plc2]\r\n\r\n")
+	*(dist) = strings.ReplaceAll(*dist, "』\r\n\r\n", "』[plc2]\r\n\r\n")
+	*(dist) = strings.ReplaceAll(*dist, "！\r\n\r\n", "！[plc2]\r\n\r\n")
+	*(dist) = strings.ReplaceAll(*dist, "、\r\n\r\n", "、[plc2]\r\n\r\n")
+	*(dist) = strings.ReplaceAll(*dist, "―\r\n\r\n", "―[plc2]\r\n\r\n")
+
+	//! 改行 + コメントもPLCの対象
+	*(dist) = strings.ReplaceAll(*dist, "。\r\n;", "。[plc2]\r\n;")
+	*(dist) = strings.ReplaceAll(*dist, "」\r\n;", "」[plc2]\r\n;")
+	*(dist) = strings.ReplaceAll(*dist, "？\r\n;", "？[plc2]\r\n;")
+	*(dist) = strings.ReplaceAll(*dist, "』\r\n;", "』[plc2]\r\n;")
+	*(dist) = strings.ReplaceAll(*dist, "！\r\n;", "！[plc2]\r\n;")
+	*(dist) = strings.ReplaceAll(*dist, "、\r\n;", "、[plc2]\r\n;")
+	*(dist) = strings.ReplaceAll(*dist, "―\r\n;", "―[plc2]\r\n;")
+}
+
+func addLR(dist *string) {
+	//! 改行1このみの場合ににタグを仕込む(ダブル改行後にplcが仕込まれている前提)
+	*(dist) = strings.ReplaceAll(*dist, "。\r\n", "。[lr]\r\n")
+	*(dist) = strings.ReplaceAll(*dist, "」\r\n", "」[lr]\r\n")
+	*(dist) = strings.ReplaceAll(*dist, "？\r\n", "？[lr]\r\n")
+	*(dist) = strings.ReplaceAll(*dist, "、\r\n", "、[lr]\r\n")
+}
+
+func addMWindowMacro(dist *string) {
+	//! セリフである"人名「.*」"にタグを仕込む(正規表現)
+	rep := regexp.MustCompile(`(#.*\n)`)
+
+	//! "人名「"をキャッチしたときの処理
+	findDialog := func(s string) string {
+		name := strings.Split(s, "#")[1]
+		name = strings.ReplaceAll(name, "\r\n", "")
+		f_str := "[show_nametag name=\"%s\" visible=true]\r\n"
+		/*
+			for i := 0; i < len(MAIN_CHARACTER_NAMES); i++ {
+				if MAIN_CHARACTER_NAMES[i] == name {
+					f_str = "[shownametag name=\"%s\" visible=true]\r\n"
+					break
+				}
+			}
+		*/
+		tagedDialog := fmt.Sprintf(f_str, name)
+		return tagedDialog
+	}
+
+	*dist = rep.ReplaceAllStringFunc(*dist, findDialog)
+}
